c2f4d586fb98f436ce56d7a1f5cf62f63a3ac024
radhikadgit <radhika1490@gmail.com>
2024-03-27 11:35:01 -0500

    Palindrome code

--- src/com/javaPractiseQuestions/Palindrome.java
+++ src/com/javaPractiseQuestions/Palindrome.java
@@ -1,30 +1,35 @@
 package com.javaPractiseQuestions;
 
+import java.util.Scanner;
+
 public class Palindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//Palindrome Program
-		
-		int num = 878;
+
+		// Palindrome Program
+		// int num = 878;
+		// enter number from Scanner class
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int num = scan.nextInt();
+
 		int reverse = 0;
 		int temp = num;
-		
-		while(num>0)
-		{
-			int x = num % 10;
-			reverse = reverse *10 + x;
-			num = num/10;
+
+		while (num > 0) {
+			reverse = reverse * 10 + num % 10;
+			num = num / 10;
+
 		}
-		if(reverse == temp) {
-			
-		    System.out.println("Given number is Palindrome "+ reverse);
-		
-		}else {
-			System.out.println("Given number is not Palindrome "+ reverse);
+		if (reverse == temp) {
+
+			System.out.println("Given number is Palindrome " + reverse);
+
+		} else {
+			System.out.println("Given number is not Palindrome " + reverse);
 		}
-			
+
 	}
 
 }
--- src/com/javaPractiseQuestions/PalindromeWord.java
+++ src/com/javaPractiseQuestions/PalindromeWord.java
@@ -1,11 +1,16 @@
 package com.javaPractiseQuestions;
 
+import java.util.Scanner;
+
 public class PalindromeWord {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		String text = "mom";
+		//String text = "mom";
+		//enter a word from scanner class
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a word ");
+		String text = scan.next();
 		String result = "" ;
 		
 		for(int i=text.length()-1; i>=0; i--) {
@@ -18,10 +23,10 @@ public class PalindromeWord {
 		
 		
 		if(text.equals(result)) {
-			System.out.println("Palindrome word " + result);
+			System.out.println("Palindrome word: " + result);
 		}else
 		{
-			System.out.println("Not Palindrome word " + result);
+			System.out.println("Not Palindrome word: " + result);
 		}
 
 	}
